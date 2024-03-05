9b3f56c47c224b42e35ed8aa2c0d3fa4806ae30e
Avinash Pandey <avinash.12009084@lpu.in>
2024-03-05 23:42:51 +0530

    Socket.io server setup done

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/auth.routes.js";
 import messagesRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-const app = express();
+import { app, server } from "./socket/socketServer.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 3001;
@@ -33,7 +33,7 @@ app.use("/api/users", userRoutes);
 //   res.send("Server is running");
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running at ${PORT}`);
 });
