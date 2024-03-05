2f13e7e067cef9aad7370193960e97c308945792
Avinash Pandey <avinash.12009084@lpu.in>
2024-03-05 16:38:40 +0530

    Send Message Added

--- client/src/components/messageContainer/MessagesInput.jsx
+++ client/src/components/messageContainer/MessagesInput.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { GrSend } from "react-icons/gr";
+import useSentMessage from "../../hooks/useSentMessage";
 
 function MessagesInput() {
-  const handleSubmit = async (e) => {};
+  const [message, setMessage] = useState("");
+  const { isLoading, sendMessage } = useSentMessage();
+  const handleSubmit = async (e) => {
+    e.prevebtDefault();
+
+    if (!message) return;
+    await sendMessage(message);
+    setMessage("");
+  };
   return (
     <form className="px-4 my-3" onSubmit={handleSubmit}>
       <div className="w-full relative">
@@ -10,12 +19,18 @@ function MessagesInput() {
           type="text"
           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-800 border-gray-700 text-white"
           placeholder="Type your Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         />
         <button
           type="submit"
           className="absolute inset-y-0 end-0 flex items-center pe-3 rounded-full"
         >
-          <GrSend className="h-6 w-6" />
+          {isLoading ? (
+            <span className="loading loading-spinner"></span>
+          ) : (
+            <GrSend className="h-6 w-6 text-gray-300" />
+          )}
         </button>
       </div>
     </form>
---
+++ client/src/hooks/useGetMessages.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+function useGetMessages() {
+  const [isLoading, setIsLoading] = useState(false);
+  return {};
+}
+
+export default useGetMessages;
