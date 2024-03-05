e1ff0db549b08edb4a5f5e6ed60262043239dbde
Avinash Pandey <avinash.12009084@lpu.in>
2024-03-06 01:23:19 +0530

    Modified

--- server/server.js
+++ server/server.js
@@ -37,9 +37,10 @@ app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
-// app.get("/", (req, res) => {
-//   res.send("Server is running");
-// });
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
